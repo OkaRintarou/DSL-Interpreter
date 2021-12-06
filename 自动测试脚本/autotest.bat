ad97d4ced350a86d089d97f1418bc71f012ad8e8
OkaRintarou <okarintarou@outlook.com>
2021-12-06 11:10:01 +0800

    添加了新的测试

--- 自动测试脚本/autotest.bat
+++ 自动测试脚本/autotest.bat
@@ -3,11 +3,11 @@ chcp 65001
 %视情况保留cd语句，此语句仅为进入可执行文件目录%
 cd ../可执行文件/net6.0
 %以下对应程序的3个参数，根据需求修改位置%
-set script="script.txt"
+set script="script1.txt"
 set userName="Jack"
 set data="data.txt"
 %此为模拟用户输入的内容%
-set input="test2.txt"
+set input="test3.txt"
 "DSL Interpreter" -f %script% -u %userName% -d %data% < %input%
 pause
 exit
